6e4160a6ed7eab4896a2ae21ea9ee6ed2ee887c6
Davi <davisilvadias0@gmail.com>
2026-04-14 20:26:51 -0300

    criei classes Usuario, Doador e Beneficiario

---
+++ src/model/Beneficiario.java
@@ -0,0 +1,10 @@
+public class Beneficiario extends Usuario {
+    private String tipo;
+    private int prioridade;
+
+    public Beneficiario(int id, String nome, String telefone, String email, String endereco, String tipo, int prioridade) {
+        super(id, nome, telefone, email, endereco);
+        this.tipo = tipo;
+        this.prioridade = prioridade;
+    }
+}
---
+++ src/model/Usuario.java
@@ -0,0 +1,24 @@
+public class Usuario {
+    private int id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String endereco;
+
+    public Usuario(int id, String nome, String telefone, String email, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = endereco;
+    }
+
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
